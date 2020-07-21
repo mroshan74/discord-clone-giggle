b3b6bfcbe1900ca46326925a4b2629241fe354f7
mroshan74 <mroshan74@gmail.com>
2020-07-21 17:48:46 +0530

    fixes

--- client/src/components/posts/PostCreate.js
+++ client/src/components/posts/PostCreate.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import Dropzone from 'react-dropzone'
 import Avatar from '@material-ui/core/Avatar'
